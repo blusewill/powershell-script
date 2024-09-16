712526d7ed73b5ec099f1f56641c9e5277c00eac
Justawildwolf <blusewillstudio@protonmail.com>
2024-09-16 22:28:30 +0800

    Update to make it work on mutilanguage system

--- Windows_Key_Script/KMS_Checker.ps1
+++ Windows_Key_Script/KMS_Checker.ps1
@@ -6,29 +6,16 @@ Out-File -InputObject $result -FilePath "$PWD\result.txt"
 # Read the content of the result.txt file
 $resultContent = Get-Content -Path "$PWD\result.txt" -Raw
 
-# Define the patterns to check
-$patterns = @(
-    "Validation URL: https://validation-v2.sls.microsoft.com/SLWGA/slwga.asmx"
-)
+# Define the pattern to check
+$pattern = "microsoft.com"
 
-# Initialize a flag to track if all patterns are found
-$allPatternsFound = $true
-
-# Check each pattern
-foreach ($pattern in $patterns) {
-    if ($resultContent -match $pattern) {
-        Write-Host "Verified: $pattern" -ForegroundColor Green
-    } else {
-        Write-Host "Not found or mismatch: $pattern" -ForegroundColor Red
-        $allPatternsFound = $false
-    }
-}
-
-# Final verdict
-if ($allPatternsFound) {
-    Write-Host "You are using non-kms Windows Machine Check Passed!" -ForegroundColor Green
+# Check if the pattern exists in the result content
+if ($resultContent -match $pattern) {
+    Write-Host "Verified: microsoft.com domain found in the activation details" -ForegroundColor Green
+    Write-Host "You are using a non-KMS Windows Machine. Check Passed!" -ForegroundColor Green
 } else {
-    Write-Host "You're running on a KMS Server of Activated Windows. Doing Further check" -ForegroundColor Yellow
+    Write-Host "Not found: microsoft.com domain not found in the activation details" -ForegroundColor Red
+    Write-Host "You're running on a KMS Server or Activated Windows. Doing further check..." -ForegroundColor Yellow
     $Pirate = Get-Process "KMS*" -ErrorAction SilentlyContinue
     if ($Pirate) {
         Write-Host "You're Using a Pirated Version of Windows that Might have Virus Installed!" -ForegroundColor Red
@@ -36,8 +23,8 @@ if ($allPatternsFound) {
     }
     else {
         Write-Host "Hmm... Looks like you're running a KMS Version of Windows without any Bad Process" -ForegroundColor Yellow
-        Write-Host "You might using the slmgr KMS Method or either We didn't detect the process in the background" -ForegroundColor Yellow
-        Write-Host "Consider Check the Process in the Task Manager for application that seems abnormal." -ForegroundColor Yellow
+        Write-Host "You might be using the slmgr KMS Method or we didn't detect the process in the background" -ForegroundColor Yellow
+        Write-Host "Consider Checking the Process in the Task Manager for applications that seem abnormal." -ForegroundColor Yellow
     }
 }
 
